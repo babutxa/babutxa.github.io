9e46c7a161399407cefb0d9227142b08dfb1306f
Alba Ferrer <alba.ferrer@gmail.com>
2020-09-03 09:44:02 +0200

    understanding scope

--- js/imageToCanvas.js
+++ js/imageToCanvas.js
@@ -1,7 +1,5 @@
 
-function setImageIntoCanvas(d) {
-    var c = d.getElementById("myCanvas");
+function setImageIntoCanvas(c, img) {
     var ctx = c.getContext("2d");
-    var img = d.getElementById("minecraftSkinTextureToFlip");
     ctx.drawImage(img, 10, 10, 150, 180);
 }
