fc8687f00522ed2badd2d03084bc2caac2cdb160
Alba Ferrer <alba.ferrer@gmail.com>
2020-09-03 08:50:03 +0200

    move vars into imageToCanvas.js

--- js/imageToCanvas.js
+++ js/imageToCanvas.js
@@ -1,3 +1,7 @@
+
+var myImage = document.getElementById("minecraftSkinTextureToFlip");
+var myCanvas = doucment.getElementById("mySrcCanvas");
+
 function imageToCanvas(myImage, myCanvas) {
     var myCanvasContext = myCanvas.getContext("2d");
     myCanvasContext.drawImage(myImage, 10, 10);
