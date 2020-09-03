fc6ce0e5145d27a69a99e8e5263dc214404b84b0
Alba Ferrer <alba.ferrer@gmail.com>
2020-09-03 09:52:45 +0200

    other way to get objects

--- js/imageToCanvas.js
+++ js/imageToCanvas.js
@@ -1,5 +1,7 @@
 
-function setImageIntoCanvas(c, img) {
+function setImageIntoCanvas(d, imgId, canvasId) {
+    var img = d.getElementById(imgId);
+    var c = d.getElementById(canvasId);
     var ctx = c.getContext("2d");
     ctx.drawImage(img, 10, 10, 150, 180);
 }
