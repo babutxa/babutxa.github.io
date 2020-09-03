c832eeb0b1b0da033f2647f308e6f50a0f13a262
Alba Ferrer <alba.ferrer@gmail.com>
2020-09-03 09:37:52 +0200

    learning function scopes in javascript

--- js/imageToCanvas.js
+++ js/imageToCanvas.js
@@ -1,7 +1,7 @@
 
-function setImageIntoCanvas() {
-    var c = document.getElementById("myCanvas");
+function setImageIntoCanvas(d) {
+    var c = d.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    var img = document.getElementById("minecraftSkinTextureToFlip");
+    var img = d.getElementById("minecraftSkinTextureToFlip");
     ctx.drawImage(img, 10, 10, 150, 180);
-  }
+}
