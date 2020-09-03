2535f1dbb743590740b3fd24846abdeadc8db367
Alba Ferrer <alba.ferrer@gmail.com>
2020-09-03 13:42:51 +0200

    create a dest canvas with the result

--- js/imageToCanvas.js
+++ js/imageToCanvas.js
@@ -7,17 +7,31 @@ function setImageIntoCanvas(d, imgId, canvasId) {
 };
 
 function flipSkin(d, srcCanvasId) {
-    var c = d.getElementById(srcCanvasId);
-    var ctx = c.getContext("2d");
+    var srcCanvas = d.getElementById(srcCanvasId);
+    var srcCtx = srcCanvas.getContext("2d");
+    var srcImgData = srcCtx.getImageData(0, 0, srcCanvas.width, srcCanvas.height);
 
-    var imgData = ctx.getImageData(0, 0, c.width, c.height);
+    // create dest canvas
+    var destCanvas = document.createElement('canvas');
+    destCanvas.id = "destCanvasId";
+    destCanvas.width = srcCanvas.width;
+    destCanvas.height = srcCanvas.height;
+    destCanvas.style.border = "1px solid #00d300";
+
+    var destCanvasCtx = destCanvas.getContext("2d");
+    var destImgData = destCanvasCtx.getImageData(0, 0, destCanvas.width, destCanvas.height);
+
     // invert colors
     var i;
-    for (i = 0; i < imgData.data.length; i += 4) {
-      imgData.data[i] = 255 - imgData.data[i];
-      imgData.data[i+1] = 255 - imgData.data[i+1];
-      imgData.data[i+2] = 255 - imgData.data[i+2];
-      imgData.data[i+3] = 255;
+    for (i = 0; i < srcImgData.data.length; i += 4) {
+        destImgData.data[i] = 255 - srcImgData.data[i];
+        destImgData.data[i+1] = 255 - srcImgData.data[i+1];
+        destImgData.data[i+2] = 255 - srcImgData.data[i+2];
+        destImgData.data[i+3] = 255;
     }
-    ctx.putImageData(imgData, 0, 0); 
+    ctx.putImageData(destImgData, 0, 0); 
+
+    // add destCanvas to body
+    var body = d.getElementsByTagName("body")[0];
+    body.appendChild(destCanvas);
 };
