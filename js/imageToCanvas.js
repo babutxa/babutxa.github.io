2429e3ee9837189af25fb49aee3f443a3de1720d
Alba Ferrer <alba.ferrer@gmail.com>
2020-09-07 14:13:30 +0200

    load and flip in one click

--- js/imageToCanvas.js
+++ js/imageToCanvas.js
@@ -7,6 +7,7 @@ function picSkinTextureOnChange(evt) {
         var fr = new FileReader();
         fr.onload = function () {
             document.getElementById('myImage').src = fr.result;
+            flipSkin('myImage');
         }
         fr.readAsDataURL(files[0]);
     }
@@ -181,48 +182,3 @@ function flipSkin(imgId) {
     var body = document.getElementsByTagName("body")[0];
     body.appendChild(resCanvas);
 }
-
-function flipSkin_old(imgId) {
-    var img = document.getElementById(imgId);
- 
-    // create source canvas
-    var srcCanvas = document.createElement('canvas');
-    srcCanvas.id = "srcCanvasId";
-    srcCanvas.width = img.width;
-    srcCanvas.height = img.height;
-    srcCanvas.style.border = "1px solid #d30000";
-
-    // put image into source canvas
-    var srcCtx = srcCanvas.getContext("2d");
-    srcCtx.drawImage(img, 0, 0);
-
-    // create dest canvas if necessary
-    var destCanvas =  document.getElementById('destCanvasId');
-    if (destCanvas == null || typeof(destCanvas) == 'undefined') {
-        // Create one
-        destCanvas = document.createElement('canvas');
-        destCanvas.id = "destCanvasId";
-        destCanvas.width = srcCanvas.width;
-        destCanvas.height = srcCanvas.height;
-        destCanvas.style.border = "1px solid #00d300";
-    }
-
-    var destCtx = destCanvas.getContext("2d");
-    var destImgData = destCtx.getImageData(0, 0, destCanvas.width, destCanvas.height);
-
-    // invert colors of srcCanvas and put the result int destCanvas
-    var srcImgData = srcCtx.getImageData(0, 0, srcCanvas.width, srcCanvas.height);
-
-    var i;
-    for (i = 0; i < srcImgData.data.length; i += 4) {
-        destImgData.data[i] = 255 - srcImgData.data[i];
-        destImgData.data[i+1] = 255 - srcImgData.data[i+1];
-        destImgData.data[i+2] = 255 - srcImgData.data[i+2];
-        destImgData.data[i+3] = 255;
-    }
-    destCtx.putImageData(destImgData, 0, 0); 
-
-    // add destCanvas to body
-    var body = document.getElementsByTagName("body")[0];
-    body.appendChild(destCanvas);
-};
