493232758b2a1974731622fb412f5d031c4a650f
Alba Ferrer <alba.ferrer@gmail.com>
2020-09-07 14:09:36 +0200

    moving all json to .js files

--- js/imageToCanvas.js
+++ js/imageToCanvas.js
@@ -1,3 +1,24 @@
+function picSkinTextureOnChange(evt) {
+    var tgt = evt.target || window.event.srcElement,
+        files = tgt.files;
+
+    // FileReader support
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            document.getElementById('myImage').src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    }
+
+    // Not supported
+    else {
+        // fallback -- perhaps submit the input to an iframe and temporarily store
+        // them on the server until the user's session ends.
+    }
+};
+
+
 // arguments
 // ctx : the context on which to draw the mirrored image
 // image : the image to mirror
