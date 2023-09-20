e6e76709c577bd2e3a0cd99de409ba985e00e16e
babutxa <alba.ferrer@gmail.com>
2023-09-20 16:49:11 +0200

    Update displaySchools.js

--- ProblemillasGymi/js/displaySchools.js
+++ ProblemillasGymi/js/displaySchools.js
@@ -194,7 +194,7 @@ function displayKurzPlan(schoolList, optionsLabel, optionsList, message) {
         const profileList = getProfiles(school);
         const listAsString = profileList.join(", ");
         const listItem = document.createElement("li");
-        listItem.textContent = `${school.schoolName} <br>(Profile: ${listAsString})`;
+        listItem.innerHTML = `${school.schoolName} <br>(Profile: ${listAsString})`;
         optionsLabel.textContent = message;
         optionsList.appendChild(listItem);
     } else if (profileFilter && !subjectFilter) { // only profile filter
@@ -204,7 +204,7 @@ function displayKurzPlan(schoolList, optionsLabel, optionsList, message) {
       if (subjectsList.length > 0) {
           const listItem = document.createElement("li");
           const listAsString = subjectsList.join(", ");
-          listItem.textContent = `${school.schoolName} <br>(Schwerpunktfächer: ${listAsString})`;
+          listItem.innerHTML = `${school.schoolName} <br>(Schwerpunktfächer: ${listAsString})`;
           optionsLabel.textContent = message;
           optionsList.appendChild(listItem); 
       }
@@ -214,7 +214,7 @@ function displayKurzPlan(schoolList, optionsLabel, optionsList, message) {
       if (profilesList.length > 0) {
           const listItem = document.createElement("li");
           const listAsString = profilesList.join(", ");
-          listItem.textContent = `${school.schoolName} <br>(Profile: ${listAsString})`;
+          listItem.innerHTML = `${school.schoolName} <br>(Profile: ${listAsString})`;
           optionsLabel.textContent = message;
           optionsList.appendChild(listItem); 
       }
