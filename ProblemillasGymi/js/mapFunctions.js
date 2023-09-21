ac5f49775809a8b9a5a2297f652da05219fccbd7
babutxa <alba.ferrer@gmail.com>
2023-09-21 14:52:39 +0200

    Update mapFunctions.js

--- ProblemillasGymi/js/mapFunctions.js
+++ ProblemillasGymi/js/mapFunctions.js
@@ -1,12 +1,12 @@
+var map;
+
 function initMap() {
-    var map = L.map('map').setView([47.372266770573944, 8.53810067665972], 13);  // Centered in Zurich
+    map = L.map('map').setView([47.372266770573944, 8.53810067665972], 13);  // Centered in Zurich
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    
-    var marker = L.marker([47.372266770573944, 8.53810067665972]).addTo(map);
 }
 
 function addSchoolToMap(school) {
