01f382ffb6f24ae8d260a67038e5bcf58110d35b
babutxa <alba.ferrer@gmail.com>
2023-09-21 13:43:05 +0200

    Update mapFunctions.js

--- ProblemillasGymi/js/mapFunctions.js
+++ ProblemillasGymi/js/mapFunctions.js
@@ -1,6 +1,8 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+function initMap() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
