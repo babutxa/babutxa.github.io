4a5b7710ea1ba1d5e94acce231b3ee30ef2ef06d
babutxa <alba.ferrer@gmail.com>
2023-09-22 16:24:05 +0200

    Update displaySchools.js

--- ProblemillasGymi/js/displaySchools.js
+++ ProblemillasGymi/js/displaySchools.js
@@ -137,22 +137,22 @@ function filterAndDisplayDataAdvanced() {
   
   if (!fromFilter) {
     displayKurzPlan(schoolsData, currentOptionsLabel, currentOptionsList, "Optionen:");
-    addSchoolsToMap(schoolsData, "#e74c3c");
+    addSchoolsToMap(schoolsData, "#fda172", "0.8");
   }
  
   const schoolList = getSchoolsFrom(fromFilter);
   if (fromFilter === "6Prima") {
     displayLangPlan(schoolList, currentOptionsLabel, currentOptionsList, "Untergymnasium Optionen:");  // schools with untergymi
-    addSchoolsToMap(schoolList, "#e74c3c");
+    addSchoolsToMap(schoolList, "#fda172", "1.0");
     
     const futureSchools = getSchoolsFrom("2Gymi")
     const hochGymiSchools = getFilteredSchools(futureSchools);
     displayKurzPlan(hochGymiSchools, futureOptionsLabel, futureOptionsList, "Optionen nach 2 Jahren Langgymnasium:");
-    addSchoolsToMap(hochGymiSchools, "#0492c2");
+    addSchoolsToMap(hochGymiSchools, "#0492c2", "0.5");
   } else if (fromFilter === "2Gymi" || fromFilter === "2or3Sek") {
     const filteredSchools = getFilteredSchools(schoolList);
     displayKurzPlan(filteredSchools, currentOptionsLabel, currentOptionsList, "Optionen:");
-    addSchoolsToMap(filteredSchools, "#e74c3c");
+    addSchoolsToMap(filteredSchools, "#0492c2", "1.0");
   }    
 }
 
